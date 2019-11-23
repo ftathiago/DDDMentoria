5c2492e18c4b421cd3b07a1f52e2c0ab9906ba1b
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-23 18:22:25 -0300

    Adicionado arquivo sln

--- Dominio.Venda.Test/UnitTest1.cs
+++ Dominio.Venda.Test/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace Dominio.Test
+namespace Dominio.Venda.Test
 {
     public class UnitTest1
     {
---
+++ Dominio.Venda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dominio.Venda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
