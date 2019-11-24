c9473be5b447fa03e804fa90f0297d56facf3c39
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 17:23:08 -0300

    Alteracao para o teste passar

--- Dominio.Venda/Venda.cs
+++ Dominio.Venda/Venda.cs
@@ -22,11 +22,18 @@ namespace Dominio.Venda
 
         public bool Validar()
         {
-            return Produto.Count > 0;
+            return Produto.Count > 0 && TotalVenda() > 0;
         }
 
         public int TotalVenda()
         {
+            /*
+                Percebo um problema aqui. Toda vez que eu invocar esse metódo o array será varrido.
+                Caso o array seja muito grande, a performance pode ser impactada. Pensei, então, em
+                Escrever alguma espécie de "cache", em uma classe, para evitar o recálculo.
+                Mas não sei se o DDD ou a experiência me levaram a isso. Ou ainda, se estou pensando muito à frente
+                Fugindo dos baby steps.
+            */
             var totalVenda = Produto.Sum(p => p.QuantidadeComprada * p.ValorUnitario);
             return totalVenda;
         }
