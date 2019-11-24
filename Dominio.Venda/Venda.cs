4bc073fbcf8b7e7066de996b18a880e5b5690508
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 01:26:59 -0300

    CriarVenda - Refatorações para retirar repetições

--- Dominio.Venda/Venda.cs
+++ Dominio.Venda/Venda.cs
@@ -2,10 +2,10 @@ namespace Dominio.Venda
 {
     public class Venda
     {
-        public string Cliente = "Cliente";
+        public string Cliente;
         public Venda(string cliente)
         {
-
+            Cliente = cliente;
         }
     }
 }
