78040ccb3f905406992062ccf4f121c07e9e7e61
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 12:35:31 -0300

    Refatorações para limpeza de código

--- Dominio.Venda.Test/VendaTest.cs
+++ Dominio.Venda.Test/VendaTest.cs
@@ -8,7 +8,9 @@ namespace Dominio.Venda.Test
     {
         /* 
         Toda venda tem um cliente
-        A venda, para ser válida, precisa possuir ao menos um produto
+        A venda, para ser válida, 
+            precisa possuir ao menos um produto
+            O valor total precisa ser maior que zero (qtd * vlrUnitario || vlrPromocional)
         Cada produto possui uma "Quantidade de promoção" que caso sejam vendidos produtos suficientes, será aplicado o valor promocional
         A venda só pode ser concluida após o pagamento ser aprovado
         A venda pode ter vários métodos de pagamento
@@ -16,15 +18,15 @@ namespace Dominio.Venda.Test
         [Fact]
         public void TestCriarVenda()
         {
-            Venda venda = new Venda("Cliente");
+            Venda venda = VendaFactory();
             var cliente = venda.Cliente;
-            Assert.Equal(cliente, "Cliente");
+            Assert.Equal("Cliente", cliente);
         }
 
         [Fact]
         public void TestVendaEhValidaComUmProdutoAoMenos()
         {
-            Venda venda = new Venda("Cliente");
+            Venda venda = VendaFactory();
             venda.AdicionarProduto("Produto");
 
             bool vendaEhValida = venda.Validar();
@@ -35,11 +37,16 @@ namespace Dominio.Venda.Test
         [Fact]
         public void TestVendaNaoEhValidaSemProdutos()
         {
-            Venda venda = new Venda("Cliente");
+            Venda venda = VendaFactory();
 
             bool vendaEhValida = venda.Validar();
 
             Assert.False(vendaEhValida);
+        }
+
+        private Venda VendaFactory()
+        {
+            return new Venda("Cliente");
         }
     }
 }
