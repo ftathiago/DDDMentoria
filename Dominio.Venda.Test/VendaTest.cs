b665a653718956c13d3d8dc2021bfd02e613741b
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 01:27:46 -0300

    Alteração de linguagem de dominio. sai da pespectiva do cliente para a perspectiva do dono do domínio

--- Dominio.Venda.Test/VendaTest.cs
+++ Dominio.Venda.Test/VendaTest.cs
@@ -7,10 +7,11 @@ namespace Dominio.Venda.Test
     public class VendaTest
     {
         /* 
-        Toda compra tem um cliente
-        Cada produto possui uma "Quantidade de promoção" que caso sejam comprados produtos suficientes, será aplicado o valor promocional
-        A compra só pode ser concluida após o pagamento ser aprovado
-        A compra pode ter vários métodos de pagamento
+        Toda venda tem um cliente
+        A venda, para ser válida, precisa possuir ao menos um produto
+        Cada produto possui uma "Quantidade de promoção" que caso sejam vendidos produtos suficientes, será aplicado o valor promocional
+        A venda só pode ser concluida após o pagamento ser aprovado
+        A venda pode ter vários métodos de pagamento
         */
         [Fact]
         public void TestCriarVenda()
