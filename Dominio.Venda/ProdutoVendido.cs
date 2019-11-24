bfa51c47a40b26963d6f4d4c80a3ccbc089aed9f
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 18:30:12 -0300

    Refatoracoes Para compilar

--- Dominio.Venda/ProdutoVendido.cs
+++ Dominio.Venda/ProdutoVendido.cs
@@ -5,5 +5,7 @@ namespace Dominio.Venda
         public string Descricao { get; set; }
         public int QuantidadeComprada { get; set; }
         public int ValorUnitario { get; set; }
+        public decimal QuantidadePromocional { get; set; }
+        public decimal ValorUnitarioPromocional { get; set; }
     }
 }
