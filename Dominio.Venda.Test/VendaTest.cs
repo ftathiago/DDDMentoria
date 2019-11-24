a590e38cfc97c4a362a9613c59db29b6860cc981
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 00:56:16 -0300

    Algumas regras para a Venda

--- Dominio.Venda.Test/VendaTest.cs
+++ Dominio.Venda.Test/VendaTest.cs
@@ -6,6 +6,12 @@ namespace Dominio.Venda.Test
 {
     public class VendaTest
     {
+        /* 
+        Toda compra tem um cliente
+        Cada produto possui uma "Quantidade de promoção" que caso sejam comprados produtos suficientes, será aplicado o valor promocional
+        A compra só pode ser concluida após o pagamento ser aprovado
+        A compra pode ter vários métodos de pagamento
+        */
         [Fact]
         public void CriarVenda()
         {
