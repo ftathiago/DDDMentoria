4144e26b64b2dc3e34b25983056daf135b7f9fdf
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-11-24 22:46:06 -0300

    VendaItemCalculaTotalItem - Green - Refatorar pra tirar repetição

--- Dominio.Venda/VendaItem.cs
+++ Dominio.Venda/VendaItem.cs
@@ -5,16 +5,19 @@ namespace Dominio.Venda
         public string Descricao { get; private set; }
         public decimal ValorUnitario { get; private set; }
         public decimal ValorUnitarioPromocional { get; private set; }
+
+        private decimal quantidade;
         public VendaItem(ProdutoVendido produtoVendido)
         {
             Descricao = produtoVendido.Descricao;
             ValorUnitario = produtoVendido.ValorUnitario;
             ValorUnitarioPromocional = produtoVendido.ValorUnitarioPromocional;
+            quantidade = produtoVendido.QuantidadeComprada;
         }
 
         public decimal TotalItem()
         {
-            return 5M;
+            return ValorUnitario * quantidade;
         }
     }
 }
