10172ba6cb8abd223048007c9c2c697745b22ac0
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-04 18:11:51 -0300

    Criado teste para classe de calculadora

---
+++ Dominio.Venda.Test/CalculadoraDePrecosTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace Dominio.Venda.Test
+{
+    public class CalculadoraDePrecosTest
+    {
+        [Fact]
+        public void TestCriarCalculadora()
+        {
+            //Given
+
+            //When
+
+            //Then
+        }
+    }
+}
