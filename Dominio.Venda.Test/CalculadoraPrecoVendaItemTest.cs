2a7bc84138afe2fdd81a3e2af2eaad5beb3876f5
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-04 18:22:30 -0300

    Calculadora: Criado teste para definir a interface da classe

---
+++ Dominio.Venda.Test/CalculadoraPrecoVendaItemTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Dominio.Venda.Test
+{
+    public class CalculadoraPrecoVendaItem
+    {
+        [Fact]
+        public void TestCriarCalculadora()
+        {
+            var calculadoraPrecoVendaItem = new CalculadoraPrecoVendaItem(
+                quantidadeVendida: 1.5M,
+                valorUnitario: 1.5M,
+                quantidadePromocional: 1.5M,
+                valorPromocional: 1.5M);
+
+            Assert.NotNull(calculadoraPrecoVendaItem);
+        }
+    }
+}
