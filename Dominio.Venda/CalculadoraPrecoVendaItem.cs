e57533bf66716588becec791301670f73fa57f57
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-04 22:37:25 -0300

    CalculadoraNormal: RefactorGreen

--- Dominio.Venda/CalculadoraPrecoVendaItem.cs
+++ Dominio.Venda/CalculadoraPrecoVendaItem.cs
@@ -2,16 +2,19 @@ namespace Dominio.Venda
 {
     public class CalculadoraPrecoVendaItem
     {
+        private readonly decimal quantidadeVendida;
+        private readonly decimal valorUnitario;
         public CalculadoraPrecoVendaItem(decimal quantidadeVendida, decimal valorUnitario,
             decimal quantidadePromocional, decimal valorPromocional,
             FormaDePagamento formaDePagamento)
         {
-
+            this.quantidadeVendida = quantidadeVendida;
+            this.valorUnitario = valorUnitario;
         }
 
         public decimal Calcular()
         {
-            return 55;
+            return quantidadeVendida * valorUnitario;
         }
     }
 }
