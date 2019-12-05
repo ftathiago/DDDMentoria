4a24e2f73117a8808355a26843c8f5295a9d350d
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-04 22:41:44 -0300

    CalculadoraNormal: Refatorar Green

--- Dominio.Venda/CalculadoraPrecoVendaItem.cs
+++ Dominio.Venda/CalculadoraPrecoVendaItem.cs
@@ -4,16 +4,20 @@ namespace Dominio.Venda
     {
         private readonly decimal quantidadeVendida;
         private readonly decimal valorUnitario;
+        private readonly decimal quantidadePromocional;
         public CalculadoraPrecoVendaItem(decimal quantidadeVendida, decimal valorUnitario,
             decimal quantidadePromocional, decimal valorPromocional,
             FormaDePagamento formaDePagamento)
         {
             this.quantidadeVendida = quantidadeVendida;
             this.valorUnitario = valorUnitario;
+            this.quantidadePromocional = quantidadePromocional;
         }
 
         public decimal Calcular()
         {
+            if (quantidadePromocional <= quantidadeVendida)
+                return 0;
             return quantidadeVendida * valorUnitario;
         }
     }
