9dc82a4e71521c47b2d7900a085e274445990306
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-11 00:25:17 -0300

    Atribuição pela direita: RED

--- Dominio.Venda.Test/ValueObjects/ValorUnitarioTest.cs
+++ Dominio.Venda.Test/ValueObjects/ValorUnitarioTest.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using Dominio.Venda.ValueObjects;
 
 namespace Dominio.Venda.Test.ValueObjects
 {
@@ -10,6 +11,15 @@ namespace Dominio.Venda.Test.ValueObjects
             var valorUnitario = new ValorUnitario(10.01M);
 
             Assert.NotNull(valorUnitario);
+        }
+
+        [Fact]
+        public void TestAtribuicaoDireita()
+        {
+            const decimal valorEsperado = 10.00M;
+            ValorUnitario valorUnitario = valorEsperado;
+
+            Assert.Equal(valorEsperado, valorUnitario.Value);
         }
     }
 }
