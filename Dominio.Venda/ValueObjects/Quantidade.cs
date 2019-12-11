6f2b074308b01f602879ef610395780bdb994fc7
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-11 12:03:27 -0300

    QuantidadeVO-atribuicao direita: RED  compilando

--- Dominio.Venda/ValueObjects/Quantidade.cs
+++ Dominio.Venda/ValueObjects/Quantidade.cs
@@ -1,10 +1,19 @@
+using System;
+
 namespace Dominio.Venda.ValueObjects
 {
     public class Quantidade
     {
+        public decimal Value => _value;
+        private decimal _value;
         public Quantidade(decimal quantidade)
         {
 
+        }
+
+        public static implicit operator Quantidade(decimal v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
