f8cc59e5224ae0eef53bccd17d13e323bb8eef93
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-11 09:08:58 -0300

    QuantidadeVO: RED

---
+++ Dominio.Venda.Test/ValueObjects/QuantidadeTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace Dominio.Venda.Test.ValueObjects
+{
+    public class QuantidadeTest
+    {
+        [Fact]
+        public void TestCriarQuantidade()
+        {
+            Quantidade quantidade = new Quantidade(0.001M);
+
+            Assert.NotNull(quantidade);
+        }
+    }
+}
