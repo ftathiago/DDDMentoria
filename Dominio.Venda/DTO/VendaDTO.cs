903cef53d8e616588af091f7be92be26baff732c
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-11 00:40:11 -0300

    Refatoração: retirar warning

--- Dominio.Venda/DTO/VendaDTO.cs
+++ Dominio.Venda/DTO/VendaDTO.cs
@@ -4,9 +4,9 @@ namespace Dominio.Venda.DTO
 {
     public class VendaDTO
     {
-        public ClienteDTO Cliente { get; set; }
+        public ClienteDTO? Cliente { get; set; }
         public FormaDePagamento FormaDePagamento { get; set; }
 
-        public List<VendaItemDTO> Itens { get; set; }
+        public List<VendaItemDTO>? Itens { get; set; }
     }
 }
