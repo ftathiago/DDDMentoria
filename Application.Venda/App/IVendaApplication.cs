55e2c2a61e9e04021f44d434fadcc6089c71eef8
Francisco Thiago de Almeida <ftathiago@gmail.com>
2019-12-14 22:42:08 -0300

    ProcessarVenda: RED

--- Application.Venda/App/IVendaApplication.cs
+++ Application.Venda/App/IVendaApplication.cs
@@ -1,7 +1,9 @@
+using CrossCutting.Models;
+
 namespace Application.Venda.App
 {
     public interface IVendaApplication
     {
-
+        bool ProcessarVenda(VendaDTO vendaDTO);
     }
 }
--- Application.Venda/App/Impl/VendaApplication.cs
+++ Application.Venda/App/Impl/VendaApplication.cs
@@ -1,7 +1,12 @@
+using CrossCutting.Models;
+
 namespace Application.Venda.App.Impl
 {
     public class VendaApplication : IVendaApplication
     {
-
+        public bool ProcessarVenda(VendaDTO vendaDTO)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
