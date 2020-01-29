4f1ae3c5a0f545ffa741a2157526cff63de9b112
Francisco Thiago de Almeida <ftathiago@gmail.com>
2020-01-29 13:01:42 -0300

    Retirar warnings do VendaDTO

--- Dominio.Venda/DTO/VendaDTO.cs
+++ Dominio.Venda/DTO/VendaDTO.cs
@@ -5,6 +5,11 @@ namespace Dominio.Venda.DTO
 {
     public class VendaDTO
     {
+        public VendaDTO()
+        {
+            Cliente = new ClienteDTO(string.Empty);
+            Itens = new List<VendaItemDTO>();
+        }
         public ClienteDTO Cliente { get; set; }
         public FormaDePagamento FormaDePagamento { get; set; }
         public List<VendaItemDTO> Itens { get; set; }
