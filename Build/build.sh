450cfcd937340d902bddeffb467e88b60c1a860d
Francisco Thiago de Almeida <ftathiago@gmail.com>
2020-02-04 21:38:57 -0300

    Ainda não envia o coverage para o sonar

--- Build/build.sh
+++ Build/build.sh
@@ -3,16 +3,13 @@
 XML_DIR="../.artifacts/Coverage/"
 REPORT_DIR="../.artifacts/Report/"
 HISTORY_DIR="../.artifacts/History/"
-
-docker run -d --name sonarqube -p 9000:9000 sonarqube
+PROJECT_SLN = '../DDDMentoria.sln'
 
 sudo rm $XML_DIR -rf
 sudo rm $REPORT_DIR -rf
 
-dotnet sonarscanner begin /k:"DDDMentoria" /d:sonar.host.url=http://localhost:9000 
-sudo dotnet clean "../DDDMentoria.sln"
-sudo dotnet build "../DDDMentoria.sln"
-sudo dotnet test "../Venda.Dominio.Test/Venda.Dominio.Test.csproj" /p:CollectCoverage=true /p:CoverletOutput=$XML_DIR /p:CoverletOutputFormat=cobertura /p:CoverletOutput="${XML_DIR}Venda.Dominio.Test.xml" /p:Exclude='[xunit.*]*'
-sudo dotnet test "../Venda.Application.Test/Venda.Application.Test.csproj" /p:CollectCoverage=true /p:CoverletOutput=$XML_DIR /p:CoverletOutputFormat=cobertura /p:CoverletOutput="${XML_DIR}Venda.Application.Test.xml" /p:Exclude='[xunit.*]*'
-dotnet sonarscanner end
+dotnet clean $PROJECT_SLN
+dotnet build $PROJECT_SLN
+dotnet test "../Venda.Dominio.Test/Venda.Dominio.Test.csproj" /p:CollectCoverage=true /p:CoverletOutput=$XML_DIR /p:CoverletOutputFormat=cobertura /p:CoverletOutput="${XML_DIR}Venda.Dominio.Test.xml" /p:Exclude='[xunit.*]*'
+dotnet test "../Venda.Application.Test/Venda.Application.Test.csproj" /p:CollectCoverage=true /p:CoverletOutput=$XML_DIR /p:CoverletOutputFormat=cobertura /p:CoverletOutput="${XML_DIR}Venda.Application.Test.xml" /p:Exclude='[xunit.*]*'
 sudo ./tools/reportgenerator -reports:"${XML_DIR}*.xml" -targetdir:$REPORT_DIR -reporttypes:"HTML;HTMLSummary" -historydir:$HISTORY_DIR
--- Build/sonar.sh
+++ Build/sonar.sh
@@ -8,11 +8,13 @@ HISTORY_DIR="../.artifacts/History/"
 
 sudo rm $XML_DIR -rf
 sudo rm $REPORT_DIR -rf
-dotnet tool install -sonarscanner
-dotnet sonarscanner begin /k:"DDDMentoria" /d:sonar.host.url=http://localhost:9000
-#sudo dotnet clean "../DDDMentoria.sln"
-dotnet clean
-dotnet build "../DDDMentoria.sln"
+
+dotnet tool install --global -sonarscanner --version 4.8.0
 dotnet test "../Venda.Dominio.Test/Venda.Dominio.Test.csproj" /p:CollectCoverage=true  /p:CoverletOutputFormat=opencover 
 dotnet test "../Venda.Application.Test/Venda.Application.Test.csproj" /p:CollectCoverage=true  /p:CoverletOutputFormat=opencover
-dotnet sonarscanner end
+
+
+#dotnet sonarscanner begin /k:"DDDMentoria" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="**/coverage.opencover.xml" /d:sonar.coverage.exclusions="**Test.cs"
+dotnet sonarscanner begin /k:"DDDMentoria" /d:sonar.host.url=http://localhost:9000 /d:sonar.login=admin /d:sonar.password=admin
+dotnet build "../DDDMentoria.sln"
+dotnet sonarscanner end /d:sonar.login=admin /d:sonar.password=admin
